35502fbb10d1e489d21bfc5ad89b4bc1dbec0910
Manish Singh Tomar <manishtomar00mt@gmail.com>
2025-04-10 13:16:07 +0530

    https added

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const API_KEY = "2fd67d752650421790c4be5eaf02c7e4";
-const url = "http://newsapi.org/v2/everything?q=";
+const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener("load",() => fetchNews("india"));
 
